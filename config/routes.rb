d305b2e7981a784339f6ad6776ca5ebcf77a09f6
jayaram87 <jayaramraja1987@gmail.com>
2016-06-24 04:57:56 +0000

    completed ajax

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get "homepage/index"
   devise_for :users
   resources :users, only: [:show] do
-    resources :items, only: [:new, :create]
+    resources :items, only: [:create, :destroy]
   end
   root 'users#show'
 end
